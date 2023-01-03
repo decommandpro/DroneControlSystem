8d706120673146759d7561c811a8b2995b0bfb19
decommandpro <93335212+decommandpro@users.noreply.github.com>
2023-01-03 15:30:25 +0100

    updated

--- DroneComputer.lua
+++ DroneComputer.lua
@@ -1,5 +1,5 @@
 local bluenet = require("bluenet")
-bluenet.open("ws://2.tcp.eu.ngrok.io:11698", "drone")
+bluenet.open("ws://6.tcp.eu.ngrok.io:17144", "drone")
 
 local drone = peripheral.find("drone_interface")
 
@@ -7,18 +7,19 @@ local function scan(radius)
     local pos = drone.getDronePositionVec()
 
     local blocks = {}
-    
+
     for x=-radius, radius do
         for y=-radius, radius do
             for z=-radius, radius do
-                repeat until drone.isActionDone()
                 drone.clearArea()
                 drone.addArea(pos.x+x, pos.y+y, pos.z+z)
                 drone.setAction("condition_block")
-                repeat until drone.isActionDone()
+                while not drone.isActionDone() do sleep() end
                 local state = drone.evaluateCondition()
-                local blockName = "minecraft_stone"
-                table.insert(blocks, {name = blockName, x = pos.x+x, y = pos.y+y, z = pos.z+z})
+                if state then
+                    local blockName = "minecraft_stone"
+                    table.insert(blocks, {name = blockName, x = pos.x+x, y = pos.y+y, z = pos.z+z})
+                end
             end
         end
     end
@@ -27,12 +28,14 @@ end
 
 
 while true do
-    local id, message = bluenet.receive()
+    local id, message = bluenet.receive("world")
     print(id)
     if message.type == "scan" then
+    print("scanin")
         local blocks = scan(message.radius)
-        
-        local message = {blocks}
+
+        local message = {blocks = blocks}
         bluenet.send("world", message, "world")
+        sleep(0.1)
     end
 end
--- WorldComputer.lua
+++ WorldComputer.lua
@@ -1,17 +1,25 @@
 local bluenet = require("bluenet")
 
-bluenet.open("ws://2.tcp.eu.ngrok.io:11698", "world")
+bluenet.open("ws://6.tcp.eu.ngrok.io:17144", "world")
 
 while true do
     repeat
+        print("awaiting scan request")
         local id, message = bluenet.receive("scan")
+        print("received")
         local scanRadius = message
-        bluenet.send("drone", {type = "scan", radius = scanRadius})
+        sleep(0.1)
+        bluenet.send("drone", {type = "scan", radius = scanRadius}, "world")
+        print("send drone request")
         local id, message = bluenet.receive("world")
+        print("received ")
         for i, b in pairs(message.blocks) do
             --print(i.."  "..b)
             local block = string.gsub(b.name, "_", ":")
-            local success, out = commands.exec("setblock "..b.x.." "..b.y.." "..b.z.." "..block)
+
+            local success, out = commands.exec("setblock "..tostring(math.floor(b.x)).." "..tostring(math.floor(b.y)+100).." "..tostring(math.floor(b.z)).." "..block)
+            --print(success)
+            --print(table.concat(out, ", "))
         end
     until true
 end
